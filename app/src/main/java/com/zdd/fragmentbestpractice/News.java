af2403fc518df36c9d33f2f6c9297a685ee0e7ed
zhudedian <1175488190@qq.com>
2018-08-12 14:00:27 +0800

    first commit

---
+++ app/src/main/java/com/zdd/fragmentbestpractice/News.java
@@ -0,0 +1,25 @@
+package com.zdd.fragmentbestpractice;
+
+/**
+ * Created by zhudedian on 2018/8/12.
+ */
+
+public class News {
+    private String title;
+    private String content;
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
